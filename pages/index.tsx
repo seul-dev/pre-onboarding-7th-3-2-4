704d667f0e8afc8c467cd3d768a7d258c0b32fc8
seul-dev <seul.dev@gmail.com>
2022-11-12 22:49:42 +0900

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className='bg-green-600'>
+      <h1>괜찮아?</h1>
+    </div>
+  );
+}
